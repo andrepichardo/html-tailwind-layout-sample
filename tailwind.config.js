b4c8883858ff454e70722b875f4e787aa1a19873
andrepichardo <36863134+andrepichardo@users.noreply.github.com>
2022-05-15 20:09:34 -0400

    feat: tailwindcss 3.0 update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,7 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    container: {
-      center: true,
-      padding: '2rem'
-
-    },
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
-}
+};
